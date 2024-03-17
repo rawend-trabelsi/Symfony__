af4985579d70ef061e315ea09522617e889d1eb0
rawend-trabelsi <147106657+rawend-trabelsi@users.noreply.github.com>
2024-03-17 11:58:25 +0100

    Add files via upload

---
+++ tp3/src/Controller/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function home(): Response
+    {
+        $articles = ['Article 1', 'Article 2', 'Article 3'];
+        return $this->render('articles/index.html.twig', ['articles' => $articles]);
+    }
+}
