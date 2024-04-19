f2b86a8ba0ee28c3ccb5fc7d3163b9d640738292
rawend-trabelsi <147106657+rawend-trabelsi@users.noreply.github.com>
2024-04-20 01:34:30 +0200

    Add files via upload

---
+++ tp5 rawend/src/Controller/IndexController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Controller;
+use App\Form\ArticleType;
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        $articles = $this->entityManager->getRepository(Article::class)->findAll();
+
+        return $this->render('articles/index.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
+
+    /**
+     * @Route("/article/save", name="save_article", methods={"GET", "POST"})
+     */
+    public function save(): Response
+    {
+        $article = new Article();
+        $article->setNom('Article 3');
+        $article->setPrix(4000);
+
+        $this->entityManager->persist($article);
+        $this->entityManager->flush();
+
+        return $this->redirectToRoute('home');
+    }
+
+    /**
+     * @Route("/articles", name="articles")
+     */
+    public function showArticles(Request $request): Response
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class,$article);
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+            $this->entityManager->persist($article);
+            $this->entityManager->flush();
+            return $this->redirectToRoute('home');
+        }
+        
+        return $this->render('articles/new.html.twig',['form' => $form->createView()]);
+    }
+     
+
+    /**
+     * @Route("/article/edit/{id}", name="edit_article", methods={"GET", "POST"})
+     */
+    /**
+* @Route("/article/edit/{id}", name="edit_article")
+* Method({"GET", "POST"})
+*/
+public function edit(Request $request, $id)
+    {
+        $article = $this->entityManager->getRepository(Article::class)->find($id);
+        
+        if (!$article) {
+            throw $this->createNotFoundException('Article non trouvé');
+        }
+
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->flush();
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('articles/edit.html.twig', ['form' => $form->createView()]);
+    }
+   
+   
+    
+    /**
+     * @Route("/article/delete/{id}", name="delete_article", methods={"POST"})
+     */
+    public function delete(Request $request, $id) {
+        $article = $this->entityManager->getRepository(Article::class)->find($id);
+        
+        if (!$article) {
+            throw $this->createNotFoundException('Article non trouvé');
+        }
+    
+        $entityManager = $this->entityManager;
+        $entityManager->remove($article);
+        $entityManager->flush();
+    
+        return $this->redirectToRoute('home'); // Rediriger vers la page d'accueil ou une autre page après la suppression
+    }
+    
+/**
+ * @Route("/article/{id}", name="article_details")
+ */
+public function details($id) {
+    $article = $this->entityManager->getRepository(Article::class)->find($id);
+    
+    if (!$article) {
+        throw $this->createNotFoundException('Article non trouvé');
+    }
+
+    return $this->render('articles/show.html.twig', ['article' => $article]);
+}
+
+}
---
+++ tp5 rawend/src/Entity/Article.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ */
+class Article
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(
+     *      min = 5,
+     *      max = 50,
+     *      minMessage = "Le nom d'un article doit comporter au moins {{ limit }} caractères",
+     *      maxMessage = "Le nom d'un article ne peut pas dépasser {{ limit }} caractères"
+     * )
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=2)
+     * @Assert\GreaterThan(
+     *      value = 0,
+     *      message = "Le prix d’un article doit être supérieur à 0"
+     * )
+     */
+    private $prix;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+}
